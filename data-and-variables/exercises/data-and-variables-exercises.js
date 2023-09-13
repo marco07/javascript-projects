c95272521213c17fb4fa9a989261b5a2ce633339
Samlitolo <samlitolo@gmail.com>
2023-09-12 22:31:20 -0500

    exer
    Declare and assign the variables below

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,5 +1,16 @@
 // Declare and assign the variables below
 
+let Name_of_the_space_shuttle;
+Name_of_the_space_shuttle="Determination";
+console.log(Name_of_the_space_shuttle);
+
+let Shuttle_Speed;
+Shuttle_Speed="17,500";
+console.log(Shuttle_Speed +" (mph)");
+
+let Distance_to_Mars;
+Distance_to_Mars="225,000,000";
+console.log(Distance_to_Mars +" (Km)");
 // Use console.log to print the 'typeof' each variable. Print one item per line.
 
 // Calculate a space mission below
